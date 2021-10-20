e2b48d817741f11d8187a3d3411e4bfdc5bd7a88
pratesbruno <bruno.sp.prates@gmail.com>
2021-10-20 14:25:12 -0300

    Changing single quotes to double quotes in response.

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import json
 import requests
 
 def extrair_infos_carteirinha(event=None):
-    
-    # Definir credenciais GCP
+    """Função lambda que extrai as informações das carteirinhas de plano de saúde.
+       Para funcionar, deve receber um evento que contém o path da carteirinha no S3."""
+
+    # Definir credenciais GCP a partir do arquivo .json
     path_atual = os.getcwd()
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path_atual + '/credenciais-gcp.json'
     
-    # Criar df_op_planos de arquivo csv
+    # Criar df_operadoras_planos a partir do arquivo csv
     df_operadoras_planos = pd.read_csv(path_atual + '/base-operadoras-planos.csv',delimiter=';')
 
     # Bucket name - passar como argumento?
@@ -28,13 +30,14 @@ def extrair_infos_carteirinha(event=None):
     carteirinha_s3.extrair_operadora_plano_num_carteirinha()
 
     response = {
-        'operadora' : carteirinha_s3.operadora,
-        'score_operadora' : carteirinha_s3.score_operadora,
-        'plano' : carteirinha_s3.plano,
-        'id_plano' : carteirinha_s3.id_plano,
-        'score_plano' : carteirinha_s3.score_plano,
-        'num_carteirinha' : carteirinha_s3.num_carteirinha
+        "operadora" : carteirinha_s3.operadora,
+        "score_operadora" : carteirinha_s3.score_operadora,
+        "plano" : carteirinha_s3.plano,
+        "id_plano" : carteirinha_s3.id_plano,
+        "score_plano" : carteirinha_s3.score_plano,
+        "num_carteirinha" : carteirinha_s3.num_carteirinha
     }
 
     print(response)
+    print(json.loads(response))
     return str(response)
